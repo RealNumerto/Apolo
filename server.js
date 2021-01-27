459f793c68d0d832eba1c77f68a3230476f626e3
Glitch (apolobalkanbot) <none>
2021-01-27 13:20:56 +0000

    💳🏨 Checkpoint ./server.js:25375062/232

--- server.js
+++ server.js
@@ -239,7 +239,7 @@ bot.Command({
   name: "pitaj",
   code: `
 $title[$username[]#$discriminator[$authorID] je postavio/la pitanje]
-$footer[Pitanje postavio/la $username[]#$discriminator[$authorID];$userAvatar[$authorID]]
+$footer[Pitanje postavio/la $username[]#$discriminator[$authorID];$userAvatar[userID;png;true;512]]
 $color[772773]
 $description[
 $addField[Odgovor bota:;$randomText[Da;Ne;Mozda;Ne moze;Naravno;Naravno da mozes;Kako da ne;Logicno;Moguce;Mozda;Ne znam;Nije mi jasno, ponovi pitanje;Hteo si odgovor, evo ti ga xD;Dobro;Lose;Ok;Nece;Hoce;Naravno da hoce;Ne verujem;jeste;nije;nema;nikako;ma kakvi;nema tebi spasa;Mozes da se kladis u to;Numero je najjaci;kobrad te voli;super;oladi;mars;nema teorije;hoces;neces;nemas sanse;nema sanse;100%;50%;10%;20%;30%;40%;60%;70%;80%;90%;ima;nema;laze;nije tako;tako je;moze;ne moze;mozes;ne mozes;naravno da ne moze;naravno da hoce;malo;naravno da je tako;nop;dap;yep;no no;da daaaa;nema sanse;ma kakvi;si ti lud?;znaci boze];no]]
@@ -253,7 +253,7 @@ bot.Command({
    name: "procenat",
    code: `
 $title[$username[]#$discriminator[$authorID] je postavio/la pitanje]
-$footer[Pitanje postavio/la $username[]#$discriminator[$authorID];$userAvatar[$replaceText[$replaceText[$checkCondition[$findUser[$message[]]==undefined];true;$authorID];false;$findUser[$message[]]];png;true;512]]
+$footer[Pitanje postavio/la $username[]#$discriminator[$authorID];$userAvatar[userID;png;true;512]]
 $color[772773]
 $description[]
 $addField[Odgovor bota:;$random[0;100]%;no]
