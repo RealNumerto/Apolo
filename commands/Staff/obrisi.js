ee9e53d9f10266fa3df56f736a19e0d7923f4ff7
Numerto <40825586+RealNumerto@users.noreply.github.com>
2021-01-28 17:31:42 +0100

    Create obrisi.js

---
+++ commands/Staff/obrisi.js
@@ -0,0 +1,11 @@
+module.exports = ({
+name: "obrisi"
+code: `
+  $title[Poruke Obrisane!!!]
+  $color[$random[0;999999]]
+  $description[Obrisano **$message[1]** poruka]
+  $clear[$message[]]
+  $onlyIf[$message[1]!=;Pogresno napisana komanda. Probaj: ** !!obrisi (neki broj)**. () ne trebas da koristis]
+  $onlyForRoles[794558765961314314;782778612361330768;801810495354306591;782775328116047892;**:x: Nemas dozvolu da koristis ovu komandu!!!**]
+  `
+  })
