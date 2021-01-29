1cd3624b33a584d065643618f03fd72aedacd491
Numerto <40825586+RealNumerto@users.noreply.github.com>
2021-01-29 13:12:20 +0100

    Update ban.js

--- commands/Staff/ban.js
+++ commands/Staff/ban.js
@@ -1,13 +1,27 @@
 module.exports = ({
   name: "ban",
   code: `
-$channelSendMessage[$channelID[];<@$findUser[$message[1]]> je banovan/a.
-Razlog: **$messageSlice[>1]**]
-$ban[$findUser[$message[1]];$messageSlice[>1]]
-$onlyIf[$findUser[$message[1]]!=$ownerID;{title:Ban Error}{description:Dobar pokusaj. Ne mozes da banujes server vlasnika.}]
-$onlyIf[$findUser[$message[1]]!=$client[id];{title:Ban Error}{description:Banaj me ako mozes xD. Ne mogu ja da banam sam sebe.}]
-$onlyIf[$findUser[$message[1]]!=$authorID;{title:Ban Error}{description:Ne mozes da banujes sebe. LoL}]
-$argsCheck[>2;Pogresno napisana komanda. Probaj: **!!ban (@username) (razlog)**. () ne trebas da koristis]
-$onlyForRoles[782775328116047892;801810495354306591;782778612361330768;802194924539871254;**:x: Nemas dozvolu da koristis ovu komandu!!!**]
-`
+  $ban[$findUser[$message[1]];$replaceText[$replaceText[$checkCondition[$messageSlice[>1]==];true;Nije napisano];false;( By: $username[]#$discriminator[] ) - $messageSlice[>1]]]
+
+ $author[USER ID: $findUser[$message[1]]]
+ $color[576ec9]
+ $thumbnail[$userAvatar[$findUser[$message[1]]]]
+ $title[$username[$findUser[$message[1]]]#$discriminator[$findUser[$message[1]]] je banovan/a!]
+ $description[
+ Razlog: $replaceText[$replaceText[$checkCondition[$messageSlice[>1]==];true;Nije napisano];false;$messageSlice[>1]]
+ ]
+ $footer[Banovan/a od strane: *$username[]#$discriminator[]*]
+ 
+ $onlyIf[$rolePosition[$highestRole[$client[id]]]<=$rolePosition[$highestRole[$findUser[$message[1]]]];Ne mogu da banujem **$username[$findUser[$message[1]]]#$discriminator[$findUser[$message[1]]]**. Postavite role od **$username[$client[id]]** Role: **$roleName[$highestRole[$client[id]]]** iznad **$roleName[$highestRole[$findUser[$message[1]]]]** rola.]
+ 
+ $onlyIf[$findUser[$message[1]]!=$ownerID;Dobar pokušaj xD, ne mogu banati server ownera.]
+ $onlyIf[$findUser[$message[1]]!=$client[id];Ako uspeš banuj me, ne mogu banati sam sebe.]
+
+ $onlyIf[$findUser[$message[1]]!=$authorID;Ne možeš banati samog sebe]
+ $onlyIf[$userExists[$findUser[$message[1]]]==true;*$message[1]* ta osoba ne postoji. Pokušaj opet.\n\`!!ban <userID/mention> (razlog)\` - Razlog je obavezan!]
+ $onlyIf[$findUser[$message[1]]!=undefined;*$message[1]* ta osoba ne postoji. Pokušaj opet.\n\`!!ban <userID/mention> (razlog)\` - Razlog je obavezan!]
+ $onlyIf[$message[]!=;Koga trebam da banujem?\n\`!!ban <userID/mention> (razlog)\` - Razlog je obavezan!]
+ $onlyBotPerms[ban;{title:Perms Greška}{description::x:Bot Nema \`Ban Members\` Permisiju}{color:#17A589}]
+ $onlyPerms[ban;{title:Perms Greška}{description::x:Nemaš dozvolu da koristiš ovu komandu}{color:#17A589}]
+  `
 })
