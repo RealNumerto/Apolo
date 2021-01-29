8ad36823ea0d8e16533ed86d77dcd2091052626a
Numerto <40825586+RealNumerto@users.noreply.github.com>
2021-01-29 15:08:12 +0100

    Update unmute.js

--- commands/Staff/unmute.js
+++ commands/Staff/unmute.js
@@ -7,9 +7,8 @@ module.exports = ({
 Razlog: **$messageSlice[>1]**]
   $takeRole[$mentioned[1];$roleID[Muted]]
   $setUserVar[autounmute;no;$mentioned[1]]
-  $onlyIf[$getUserVar[autounmute;$mentioned[1]]==yes;**$username[$mentioned[1]]#$discriminator[$mentioned[1]]** nije mutiran/a]
   $onlyIf[$hasRole[$replaceText[$findUser[$message[]];undefined;$authorID];$roleID[Muted]]!=;Ova osoba nije mutirana]
-  $argsCheck[>2;Pogresno napisana komanda. Probaj: **!!unmute (@user) (razlog)**. () ne trebas da koristis]
+  $argsCheck[>2;Pogresno napisana komanda. Probaj: **!!unmute (@username) (razlog)**. () ne trebas da koristis]
   $onlyBotPerms[managemessages;{title:Perms Greška}{description::x:Bot Nema \`Manage Permissions\` Permisiju}{color:#17A589}]
   $onlyPerms[managemessages;{title:Perms Greška}{description::x:Nemaš dozvolu da koristiš ovu komandu}{color:#17A589}]
   `
