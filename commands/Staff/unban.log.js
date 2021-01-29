a1a9c0031cdc13bb33f046508e6d08e785c3979f
Numerto <40825586+RealNumerto@users.noreply.github.com>
2021-01-29 13:15:43 +0100

    Update unban.log.js

--- commands/Staff/unban.log.js
+++ commands/Staff/unban.log.js
@@ -4,6 +4,7 @@ module.exports = ({
   $channelSendMessage[794551111038009404;{title:User Unbanovan}{description:**$username[$findUser[$message[1]]]#$discriminator[$findUser[$message[1]]]** je unbanovan/a.
 Razlog: **$messageSlice[>1]**}{footer:Unbanovan/a od strane $username[$authorID]#$discriminator[$authorID]}]
 $argsCheck[>2;]
-$onlyForRoles[782775328116047892;801810495354306591;782778612361330768;802194924539871254;]
+$onlyBotPerms[ban;{title: }{description: }{color:#17A589}]
+$onlyPerms[ban;{title: }{description: }{color:#17A589}]
 `
 })
