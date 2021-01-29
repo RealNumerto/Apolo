5f3bf30a916c4dbe6c3cd9617f0bdfcdaa5e6081
Numerto <40825586+RealNumerto@users.noreply.github.com>
2021-01-29 15:07:51 +0100

    Update mute.js

--- commands/Staff/mute.js
+++ commands/Staff/mute.js
@@ -1,17 +1,10 @@
 module.exports = ({
 name: "mute",
 code: `
-$channelSendMessage[$channelID[];<@$mentioned[1]> je automatski unmutiran/a posle **$message[2]**]
-$takeRole[$mentioned[1];$roleID[Muted]]
-$onlyIf[$getUserVar[autounmute;$mentioned[1]]==yes;]
-$replyIn[$message[2]]
-$onlyIf[$hasRole[$mentioned[1];$roleID[Muted]]!=;]
-$channelSendMessage[$channelID[];<@$mentioned[1]> je mutiran na **$message[2]**.
+$channelSendMessage[$channelID[];<@$mentioned[1]> je mutiran/a.
 Razlog: **$messageSlice[>2]**]
 $giveRole[$mentioned[1];$roleID[Muted]]
-$setUserVar[autounmute;yes;$mentioned[1]]
-$argsCheck[>2;Pogresno napisana komanda. Probaj: **!!mute (@username) (na koliko mute(vreme)) (razlog)**. () ne trebas da koristis]
-$onlyIf[$isNumber[$replaceText[$replaceText[$replaceText[$replaceText[$message[2];h;];m;];s;];d;]]==true;Pogresno napisana komanda. Probaj: **!!mute (@username) (na koliko mute(vreme)) (razlog)**. () ne trebas da koristis]
+$argsCheck[>2;Pogresno napisana komanda. Probaj: **!!mute (@username) (razlog)**. () ne trebas da koristis]
 $onlyBotPerms[managemessages;{title:Perms Greška}{description::x:Bot Nema \`Manage Permissions\` Permisiju}{color:#17A589}]
 $onlyPerms[managemessages;{title:Perms Greška}{description::x:Nemaš dozvolu da koristiš ovu komandu}{color:#17A589}]
 `
