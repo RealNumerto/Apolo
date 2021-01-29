5594ae01adcb955d4d18a83c46797bc80d065ada
Numerto <40825586+RealNumerto@users.noreply.github.com>
2021-01-29 13:16:46 +0100

    Update userinfo.js

--- commands/Clanovi/userinfo.js
+++ commands/Clanovi/userinfo.js
@@ -1,13 +1,29 @@
 module.exports = ({
   name: "userinfo",
   code: `
-  $author[$username[$replaceText[$findUser[$message[]];undefined;$authorID]]#$discriminator[$replaceText[$findUser[$message[]];undefined;$authorID]] Info;$userAvatar[$replaceText[$findUser[$message[]];undefined;$authorID]]]
-  $description[<@$replaceText[$findUser[$message[]];undefined;$authorID]>
-  $addField[User ID;$replaceText[$findUser[$message[]];undefined;$authorID]]
-  $addField[Napravljen acc:;$creationTime[$replaceText[$findUser[$message[]];undefined;$authorID];user]
-  $creationDate[$replaceText[$findUser[$message[]];undefined;$authorID];user]]
-  $addField[Usao/la na server:;$creationTime[$replaceText[$findUser[$message[]];undefined;$authorID];member]
-  $creationDate[$replaceText[$findUser[$message[]];undefined;$authorID];member]]
-  $addField[Najveci role:;<@&$highestRole[$replaceText[$findUser[$message[]];undefined;$authorID]]>]]
-  `
+  $title[Userinfo]
+$description[** **]
+
+$addField[Da li je boostovao/la **$serverName[]**:;**$replaceText[$replaceText[$isBoosting[$mentioned[1;yes]];true;Jeste];false;Nije]**;yes]
+
+$addField[Platforma; $replaceText[$replaceText[$replaceText[$platform[$mentioned[1]];mobile; 📱 mobile];web; 💻 web];desktop; 🖥️ desktop];yes]
+
+$addField[Da li je bot:;**$replaceText[$replaceText[$isBot[$mentioned[1;yes]];false;Nije];true;Jeste]**;yes]
+
+$addField[User badges:;$getUserBadges[$mentioned[1;yes]];yes]
+
+$addField[User roles:;$memberRoles[$mentioned[1;yes]];yes]
+
+$addField[Account napravljen:;$creationDate[$mentioned[1;yes]];yes]
+
+$addField[Usao/la na server:;$creationTime[$mentioned[1;yes];member];yes]
+
+$addField[Server nickname:;<@$mentioned[1;yes]>;yes]
+
+$addField[Discord tag:;**$tag[$mentioned[1]]**;yes]
+$color[46FF00]
+$thumbnail[$userAvatar[$mentioned[1;yes];png;true;512]]
+$footer[Komandu izvrsio/la: $username[$authorID]] 
+$addTimestamp
+`
 })
