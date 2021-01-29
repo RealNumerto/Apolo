9e9725dddfa058677cc742f39274af8ba26a9864
Numerto <40825586+RealNumerto@users.noreply.github.com>
2021-01-29 14:06:51 +0100

    Update userinfo.js

--- commands/Clanovi/userinfo.js
+++ commands/Clanovi/userinfo.js
@@ -3,27 +3,26 @@ module.exports = ({
   code: `
   $title[Userinfo]
 $description[** **]
+$addField[Da li je boostovao/la **$serverName[]**:;**$replaceText[$replaceText[$isBoosting[$replaceText[$findUser[$message[]];undefined;$authorID]];true;Jeste];false;Nije]**;yes]
 
-$addField[Da li je boostovao/la **$serverName[]**:;**$replaceText[$replaceText[$isBoosting[$mentioned[1;yes]];true;Jeste];false;Nije]**;yes]
+$addField[Platforma; $replaceText[$replaceText[$replaceText[$platform[$replaceText[$findUser[$message[]];undefined;$authorID]];mobile; 📱 mobile];web; 💻 web];desktop; 🖥️ desktop];yes]
 
-$addField[Platforma; $replaceText[$replaceText[$replaceText[$platform[$mentioned[1]];mobile; 📱 mobile];web; 💻 web];desktop; 🖥️ desktop];yes]
+$addField[Da li je bot:;**$replaceText[$replaceText[$isBot[$replaceText[$findUser[$message[]];undefined;$authorID]];false;Nije];true;Jeste]**;yes]
 
-$addField[Da li je bot:;**$replaceText[$replaceText[$isBot[$mentioned[1;yes]];false;Nije];true;Jeste]**;yes]
+$addField[User značke:;$replaceText[$replaceText[$checkCondition[$getUserBadges[$replaceText[$findUser[$message[]];undefined;$authorID]]==];true;Nema];false;$getUserBadges[$replaceText[$findUser[$message[]];undefined;$authorID]]];yes]
 
-$addField[User badges:;$getUserBadges[$mentioned[1;yes]];yes]
+$addField[User roles:;$memberRoles[$replaceText[$findUser[$message[]];undefined;$authorID]];yes]
 
-$addField[User roles:;$memberRoles[$mentioned[1;yes]];yes]
+$addField[Account napravljen:;$creationDate[$replaceText[$findUser[$message[]];undefined;$authorID]];yes]
 
-$addField[Account napravljen:;$creationDate[$mentioned[1;yes]];yes]
+$addField[Usao/la na server:;$creationTime[$replaceText[$findUser[$message[]];undefined;$authorID];member];yes]
 
-$addField[Usao/la na server:;$creationTime[$mentioned[1;yes];member];yes]
-
-$addField[Server nickname:;<@$mentioned[1;yes]>;yes]
+$addField[Server nickname:;<@$replaceText[$findUser[$message[]];undefined;$authorID]>;yes]
 
 $addField[Discord tag:;**$tag[$mentioned[1]]**;yes]
 $color[46FF00]
-$thumbnail[$userAvatar[$mentioned[1;yes];png;true;512]]
-$footer[Komandu izvrsio/la: $username[$authorID]] 
+$thumbnail[$userAvatar[$replaceText[$findUser[$message[]];undefined;$authorID];png;true;512]]
+$footer[Komandu izvrsio/la: $tag[$authorID]] 
 $addTimestamp
 `
 })
