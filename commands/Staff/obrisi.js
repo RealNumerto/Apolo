8f9d4d9c2fcb175cfea8ef96cb07ab6c2cda8344
Numerto <40825586+RealNumerto@users.noreply.github.com>
2021-02-01 23:19:30 +0100

    Update obrisi.js

--- commands/Staff/obrisi.js
+++ commands/Staff/obrisi.js
@@ -6,9 +6,9 @@ module.exports = ({
  $title[Brisanje Poruka Uspešno]
  $description[Uspešno Obrisano **$message[]** Poruka U <#$channelID[]>]
  $color[#17A589]
- $onlyIf[$isNumber[$message[]]==true;{title:Greška}{description:To Nije Validan Broj!}{color:#17A589}]
- $onlyIf[$message[]!=;{title:Greška}{description:Nisi Napisao/la Koliko Poruka Oćeš Da Obrišeš}{color:#17A589}]
- $onlyBotPerms[managemessages;{title:Perms Greška}{description::x:Bot Nema \`Manage Permissions\` Permisiju}{color:#17A589}]
- $onlyPerms[managemessages;{title:Perms Greška}{description::x:Nemaš dozvolu da koristiš ovu komandu}{color:#17A589}]
+ $onlyIf[$isNumber[$message[]]==true;{title:Greška}{description:To Nije Validan Broj!}{footer:Apolo Community™}{color:ff0000}]
+ $onlyIf[$message[]!=;{title:Greška}{description:Nisi Napisao/la Koliko Poruka Oćeš Da Obrišeš}{footer:Apolo Community™}{color:ff0000}]
+ $onlyBotPerms[managemessages;{title:Perms Greška}{description::x:Bot Nema \`Manage Permissions\` Permisiju}{footer:Apolo Community™}{color:ff0000}]
+ $onlyPerms[managemessages;{title:Perms Greška}{description::x:Nemaš dozvolu da koristiš ovu komandu}{footer:Apolo Community™}{color:ff0000}]
  `
 })
