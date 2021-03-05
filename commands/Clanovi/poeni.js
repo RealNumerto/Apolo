8f3150f07e6b9b2176d4798833b1a7ff5ef9e2c0
Numerto <40825586+RealNumerto@users.noreply.github.com>
2021-03-05 15:57:06 +0100

    Update poeni.js

--- commands/Clanovi/poeni.js
+++ commands/Clanovi/poeni.js
@@ -1,7 +1,8 @@
 module.exports = ({
-  name: "poeni",
+  name: "@poeni",
   code: `
   $title[$username[$replaceText[$findUser[$message[]];undefined;$authorID]]#$discriminator[$replaceText[$findUser[$message[]];undefined;$authorID]] Poeni]
+  $footer[Brojanje je pocelo od 8.2.2021;https://media.discordapp.net/attachments/794585038812479539/796688558869446666/gif_apolo.gif]
   $color[$random[0;999999]]
   $description[
   <@$replaceText[$findUser[$message[]];undefined;$authorID]> ima: **$getUserVar[lvl;$replaceText[$findUser[$message[]];undefined;$authorID]]** poena!!!]
