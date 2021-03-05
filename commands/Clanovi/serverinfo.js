91c2ede13a3f5ef6184d0867d84ecf4c65a90576
Numerto <40825586+RealNumerto@users.noreply.github.com>
2021-03-05 16:00:44 +0100

    Update serverinfo.js

--- commands/Clanovi/serverinfo.js
+++ commands/Clanovi/serverinfo.js
@@ -2,11 +2,12 @@ module.exports = ({
   name: "serverinfo",
   code: `$title[$serverName[]]
   $description[$addField[Server Verification Level;$serverVerificationLvl[]]
-  $addField[Members Count;**$membersCount[human]**]
+  $addField[Members Count;**$sum[$membersCount[online];$membersCount[idle];$membersCount[dnd]] Clanova Online.
+  $membersCount[human] Clanova na serveru.**]
   $addField[Boost Level;$serverBoostLevel[]]
   $addField[Boost Count;$serverBoostCount[]]
   $addField[Region;$region]
-  $addField[Creation Date;$creationDate[$guildID[];guild]]
+  $addField[Creation Date;$creationDate[$guildID;guild]]
   $addField[Owner;<@$ownerID>]]
 `
 })
