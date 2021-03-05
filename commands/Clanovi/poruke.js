37b184b3db8ba633cdedc119f4378975704949d0
Numerto <40825586+RealNumerto@users.noreply.github.com>
2021-03-05 15:57:23 +0100

    Update poruke.js

--- commands/Clanovi/poruke.js
+++ commands/Clanovi/poruke.js
@@ -1,8 +1,9 @@
 module.exports = ({
-  name: "poruke",
+  name: "@poruke",
   code: `
   $title[$username[$replaceText[$findUser[$message[]];undefined;$authorID]]#$discriminator[$replaceText[$findUser[$message[]];undefined;$authorID]] broj poruka]
   $color[$random[0;999999]]
+  $footer[Brojanje je pocelo od 8.2.2021;https://media.discordapp.net/attachments/794585038812479539/796688558869446666/gif_apolo.gif]
   $description[
   Ukupno poruka poslato od strane <@$replaceText[$findUser[$message[]];undefined;$authorID]>:
   **$getUserVar[msg;$replaceText[$findUser[$message[]];undefined;$authorID]]** poruka]
