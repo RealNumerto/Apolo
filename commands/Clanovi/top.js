817b533adcf5223fc6962fa39add7d73fe8dfc42
Numerto <40825586+RealNumerto@users.noreply.github.com>
2021-03-05 15:57:59 +0100

    Create top.js

---
+++ commands/Clanovi/top.js
@@ -0,0 +1,13 @@
+module.exports = ({
+  name: "@top",
+  code: `
+  $author[Top Liste;https://media.discordapp.net/attachments/794585038812479539/796688558869446666/gif_apolo.gif]
+  $footer[Apolo Community™ | Brojanje je pocelo od 8.2.2021;https://media.discordapp.net/attachments/794585038812479539/796688558869446666/gif_apolo.gif]
+  $color[00ff00]
+  $description[$addField[Top Liste;\`poruke\` **|** \`level\` **|** \`poeni\`]
+  $addField[Komanda;Da vidis top liste sledeca je komanda: \`!!top <lista>\`]]
+  $onlyIf[$message[1]!=poeni;{execute:poeni}]
+  $onlyIf[$message[1]!=level;{execute:level}]
+  $onlyIf[$message[1]!=poruke;{execute:poruke}]
+  `
+})
