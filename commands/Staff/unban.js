ced41f05e37c640285d9d05b8145404d5a950949
Numerto <40825586+RealNumerto@users.noreply.github.com>
2021-03-06 11:53:34 +0100

    Update unban.js

--- commands/Staff/unban.js
+++ commands/Staff/unban.js
@@ -6,7 +6,7 @@ module.exports = ({
 {thumbnail:$serverIcon}{authoricon:https://cdn.discordapp.com/attachments/551653964597428225/814228124471787590/6773_Alert.png}
  
 {field:Unbanovan/a: **$tag[$findUser[$message[1]]]**:no}
-{field:Razlog:$replaceText[$replaceText[$checkCondition[$messageSlice[>2]==];true;bez razloga];false;$messageSlice[>2]]:no}
+{field:Razlog:$replaceText[$replaceText[$checkCondition[$messageSlice[>1]==];true;bez razloga];false;$messageSlice[>2]]:no}
 {field:Moderator: **$tag[$authorID]**:no}
 {field:ID-ovi:\`\`\`
 User ID = $findUser[$message[1]]
@@ -17,6 +17,7 @@ Moderator ID = $authorID\`\`\`:no}
   $channelSendMessage[$channelID[];**$username[$findUser[$message[1]]]#$discriminator[$findUser[$message[1]]]** je unbanovan/a.
 Razlog: **$messageSlice[>1]**]
 $unban[$message[1];$replaceText[$replaceText[$checkCondition[$messageSlice[>1]==];true;Nije napisano];false;( By: $username[]#$discriminator[] ) - $messageSlice[>1]]]
+$onlyIf[$isBanned[$findUser[$message[1]]]!=false;:x: User is not banned]
 $argsCheck[>2;Pogresno napisana komanda. Probaj: **!!unban (USER ID) (razlog)**. () ne trebas da koristis]
 $onlyBotPerms[ban;{title:Perms Greška}{description::x:Bot Nema \`Ban Members\` Permisiju}{color:#17A589}]
 $onlyPerms[ban;{title:Perms Greška}{description::x:Nemaš dozvolu da koristiš ovu komandu}{color:#17A589}]
