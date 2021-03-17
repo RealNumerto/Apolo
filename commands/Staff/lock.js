edb9645fad319e96f542527e23474e3e00429d5e
Numerto <40825586+RealNumerto@users.noreply.github.com>
2021-03-17 16:52:20 +0100

    Update lock.js

--- commands/Staff/lock.js
+++ commands/Staff/lock.js
@@ -15,7 +15,8 @@ module.exports = ({
   $color[00ff00]
   $description[Uspesno zakljucan kanal <#$channelID>]
   $modifyChannelPerms[$channelID;-sendmessages;782754905932562442]
-  $onlyBotPerms[administrator;{title:Perms Greška}{description::x:Bot Nema \`Administrator\` Permisiju}{footer:Apolo Community™}{color:ff0000}]
+$onlyIf[$getServerVar[regmodlogs]!={title:Greška}{description:Nije setovan kanal za logovanje. Setuj komandom: **$getServerVar[prefix]setlogs <#taguj kanal>**.}{footer:$serverName[]}{color:ff0000}]  
+$onlyBotPerms[administrator;{title:Perms Greška}{description::x:Bot Nema \`Administrator\` Permisiju}{footer:Apolo Community™}{color:ff0000}]
   $onlyPerms[administrator;{title:Perms Greška}{description::x:Nemaš dozvolu da koristiš ovu komandu}{footer:Apolo Community™}{color:ff0000}]
   
   $suppressErrors[{title:Greška?}{description:Nešto nije u redu. Ako se ovo nastavi, kontaktiraj bot developera.}{color:RED}]
