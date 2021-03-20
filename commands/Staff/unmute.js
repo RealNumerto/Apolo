dc5807e440ba4d136c64780b82b5ed6d6a0ce54c
Numerto <40825586+RealNumerto@users.noreply.github.com>
2021-03-20 22:02:25 +0100

    Update unmute.js

--- commands/Staff/unmute.js
+++ commands/Staff/unmute.js
@@ -20,13 +20,14 @@ Moderator ID = $authorID\`\`\`:no}
   $description[<@$findUser[$message[1]]> je unmutiran/a
 Razlog: **$messageSlice[>1]**]
   $unmute[$findUser[$message[1]];$roleID[Muted];$replaceText[$replaceText[$checkCondition[$messageSlice[>1]==];true;Nije napisano];false;( By: $username[]#$discriminator[] ) - $messageSlice[>2]]]
-  $setUserVar[autounmute;no;$findUser[$message[1]]]
-  $onlyIf[$hasRole[$replaceText[$findUser[$message[]];undefined;$authorID];$roleID[Muted]]!=;Ova osoba nije mutirana]
-  $argsCheck[>2;Pogresno napisana komanda. Probaj: **!!unmute (@username) (razlog)**. () ne trebas da koristis]
-$onlyIf[$getServerVar[regmodlogs]!={title:Greška}{description:Nije setovan kanal za logovanje. Setuj komandom: **$getServerVar[prefix]setlogs <#taguj kanal>**.}{footer:$serverName[]}{color:ff0000}]  
-$onlyBotPerms[manageroles;{title:Perms Greška}{description::x:Bot Nema \`Manage Roles\` Permisiju}{color:#17A589}]
-  $onlyForRoles[809542479384281119;809542278774915185;805395974759383042;805249119786369024;805225433191940116;805220500728578088;{title::x: Greška!}{description:**Nemaš dozvolu da koristiš ovu komandu!!!**}{footer:Apolo Community™}{color:ff0000}]
+  $onlyIf[$hasRole[$replaceText[$findUser[$message[]];undefined;$authorID];$roleID[Muted]]!=;{title:Greška}{description:Ova osoba nije mutirana.}{footer:$serverName[]}{color:ff0000}]
+  $argsCheck[>2;{title:Greška}{description:Pogresno napisana komanda. Probaj: **!!unmute (@username) (razlog)**. () ne trebas da koristis.}{footer:$serverName[]}{color:ff0000}]
+  $onlyIf[$getServerVar[regmodlogs]!={title:Greška}{description:Nije setovan kanal za logovanje. Setuj komandom: **$getServerVar[prefix]setlogs <#taguj kanal>**.}{footer:$serverName[]}{color:ff0000}]
+  $onlyBotPerms[manageroles;{title:Perms Greška}{description::x:Bot Nema \`Manage Roles\` Permisiju}{color:#17A589}]
+  $onlyForRoles[809542479384281119;809542278774915185;805395974759383042;805249119786369024;805225433191940116;805220500728578088;{title::x: Greška!}{description:**Nemaš dozvolu da koristiš ovu komandu!!!**}{footer:$serverName[]}{color:ff0000}]
   
   $suppressErrors[{title:Greška?}{description:Nešto nije u redu. Ako se ovo nastavi, kontaktiraj bot developera.}{color:RED}]
   `
 })
+
+//$onlyIf[$getServerVar[regmodlogs]!={title:Greška}{description:Nije setovan kanal za logovanje. Setuj komandom: **$getServerVar[prefix]setlogs <#taguj kanal>**.}{footer:$serverName[]}{color:ff0000}]///
