92f741361b609df20c73e6e69cbd719d3812ca66
Numerto <40825586+RealNumerto@users.noreply.github.com>
2021-03-20 22:02:02 +0100

    Update unlock.js

--- commands/Staff/unlock.js
+++ commands/Staff/unlock.js
@@ -11,13 +11,13 @@ module.exports = ({
 {timestamp}]
 
   $title[Unlock!!!]
-  $footer[Apolo Community™ Unlock]
+  $footer[$serverName[] Unlock]
   $color[00ff00]
   $description[Uspesno otkljucan kanal <#$channelID>]
   $modifyChannelPerms[$channelID;+sendmessages;782754905932562442]
-$onlyIf[$getServerVar[regmodlogs]!={title:Greška}{description:Nije setovan kanal za logovanje. Setuj komandom: **$getServerVar[prefix]setlogs <#taguj kanal>**.}{footer:$serverName[]}{color:ff0000}]  
-$onlyBotPerms[administrator;{title:Perms Greška}{description::x:Bot Nema \`Administrator\` Permisiju}{footer:Apolo Community™}{color:ff0000}]
-  $onlyPerms[administrator;{title:Perms Greška}{description::x:Nemaš dozvolu da koristiš ovu komandu}{footer:Apolo Community™}{color:ff0000}]
+  $onlyIf[$getServerVar[regmodlogs]!={title:Greška}{description:Nije setovan kanal za logovanje. Setuj komandom: **$getServerVar[prefix]setlogs <#taguj kanal>**.}{footer:$serverName[]}{color:ff0000}]
+  $onlyBotPerms[administrator;{title:Perms Greška}{description::x:Bot Nema \`Administrator\` Permisiju}{footer:$serverName[]}{color:ff0000}]
+  $onlyPerms[administrator;{title:Perms Greška}{description::x:Nemaš dozvolu da koristiš ovu komandu}{footer:$serverName[]}{color:ff0000}]
   
   $suppressErrors[{title:Greška?}{description:Nešto nije u redu. Ako se ovo nastavi, kontaktiraj bot developera.}{color:RED}]
   `
