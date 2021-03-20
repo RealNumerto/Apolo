42fa34a955de80bfc42a9336fb99b730a3a98e77
Numerto <40825586+RealNumerto@users.noreply.github.com>
2021-03-20 22:02:50 +0100

    Update warn.js

--- commands/Staff/warn.js
+++ commands/Staff/warn.js
@@ -20,21 +20,20 @@ $channelSendMessage[$getServerVar[regmodlogs];
  
 {field:Upozoren/a: **$tag[$mentioned[1]]**:no} 
 {field:Broj upozorenja: $sum[$getUserVar[warnings;$mentioned[1]];1]:no} 
-{field:Razlog:$replaceText[$replaceText[$checkCondition[$messageSlice[>2]==];true;bez razloga];false;$messageSlice[>2]]:no} 
+{field:Razlog:$messageSlice[>1]}
 {field:Moderator: **$tag[$authorID]**:no} 
 {field:ID-ovi:\`\`\`
 User ID = $mentioned[1]
 Moderator ID = $authorID\`\`\`:no} 
 {color:ff0000}]
 
-$onlyIf[$messageSlice[>1]!=;Obavezno je napisati razlog upozorenja.]
-$onlyIf[$mentioned[1]!=$client[id];Što bih ja dobio upozorenje?]
-$onlyIf[$mentioned[1]!=$authorID;Ne možes sam sebe da upozoriš.]
-$onlyIf[$mentioned[1]!=;Taguj nekoga da bih ga/je upozorio.]
-$onlyIf[$getServerVar[regmodlogs]!=Nije setovan kanal za logovanje. Setuj komandom: **$getServerVar[prefix]setlogs <#mention-channel>**]
+$onlyIf[$messageSlice[>1]!=;{title:Greška}{description:Obavezno je napisati razlog upozorenja.}{footer:$serverName[]}{color:ff0000}]
+$onlyIf[$mentioned[1]!=$client[id];{title:Greška}{description:Što bih ja dobio upozorenje?}{footer:$serverName[]}{color:ff0000}]
+$onlyIf[$mentioned[1]!=$authorID;{title:Greška}{description:Ne možes sam sebe da upozoriš.}{footer:$serverName[]}{color:ff0000}]
+$onlyIf[$mentioned[1]!=;{title:Greška}{description:Taguj nekoga da bih ga/je upozorio.}{footer:$serverName[]}{color:ff0000}]
 $onlyIf[$getServerVar[regmodlogs]!={title:Greška}{description:Nije setovan kanal za logovanje. Setuj komandom: **$getServerVar[prefix]setlogs <#taguj kanal>**.}{footer:$serverName[]}{color:ff0000}]
-$onlyBotPerms[managemessages;{title:Perms Greška}{description::x:Bot Nema \`Manage Messages\` Permisiju}{footer:Apolo Community™}{color:ff0000}]
-$onlyPerms[managemessages;{title:Perms Greška}{description::x:Nemaš dozvolu da koristiš ovu komandu}{footer:Apolo Community™}{color:ff0000}]
+$onlyBotPerms[managemessages;{title:Perms Greška}{description::x:Bot Nema \`Manage Messages\` Permisiju}{footer:$serverName[]}{color:ff0000}]
+$onlyPerms[managemessages;{title:Perms Greška}{description::x:Nemaš dozvolu da koristiš ovu komandu}{footer:$serverName[]}{color:ff0000}]
 
 $suppressErrors[{title:Greška?}{description:Nešto nije u redu. Ako se ovo nastavi, kontaktiraj bot developera.}{color:RED}]
 `
