79dbc6154a3b8ffe2b316cc12f7793f41c338611
Numerto <40825586+RealNumerto@users.noreply.github.com>
2021-03-20 22:00:08 +0100

    Update mute.js

--- commands/Staff/mute.js
+++ commands/Staff/mute.js
@@ -25,18 +25,18 @@ Od strane: <@$authorID>
 Razlog: \`$replaceText[$replaceText[$checkCondition[$messageSlice[>2]==];true;bez razloga];false;$messageSlice[>2]]\`
 Vreme: $message[2]**}{color:00ff00}{footer:$serverName[]}{footericon:https://media.discordapp.net/attachments/805212375133061161/807591306280304660/gif_apolo.gif}]
 $mute[$findUser[$message[1]];$roleID[Muted];$replaceText[$replaceText[$checkCondition[$messageSlice[>1]==];true;Nije napisano];false;( By: $username[]#$discriminator[] ) - $messageSlice[>2]]]
-$onlyIf[$findUser[$message[1]]!=$authorID;{title::x: Greška!}{description:**Ne mozeš mutovati sam/a sebe**}{footer:Apolo Community™}{color:ff0000}]
-$onlyIf[$findUser[$message[1]]!=;{title::x: Greška!}{description:**Nisi napisao/la koga hoces da mutiras.** \n\`$getServerVar[prefix]mute <@user> <vreme> <razlog>\`}{footer:Apolo Community™}{color:ff0000}]
-$onlyIf[$message[2]!=;{title::x: Greška!}{description:**Vreme nije napisano.** \n\`$getServerVar[prefix]mute <@user> <vreme> <razlog>\`}{footer:Apolo Community™}{color:ff0000}]
-$onlyIf[$isNumber[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$message[2];sec;];min;];hour;];days;];weeks;];day;];week;];d;];s;];w;];m;];h;]]==true;{title::x: Greška!}{description:**Netacno napisano vreme.** \n\`$getServerVar[prefix]mute <@user> <vreme> <razlog>\`}{footer:Apolo Community™}{color:ff0000}]
+$onlyIf[$findUser[$message[1]]!=$authorID;{title::x: Greška!}{description:**Ne mozeš mutovati sam/a sebe**}{footer:$serverName[]}{color:ff0000}]
+$onlyIf[$findUser[$message[1]]!=;{title::x: Greška!}{description:**Nisi napisao/la koga hoces da mutiras.** \n\`$getServerVar[prefix]mute <@user> <vreme> <razlog>\`}{footer:$serverName[]}{color:ff0000}]
+$onlyIf[$message[2]!=;{title::x: Greška!}{description:**Vreme nije napisano.** \n\`$getServerVar[prefix]mute <@user> <vreme> <razlog>\`}{footer:$serverName[]}{color:ff0000}]
+$onlyIf[$isNumber[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$message[2];sec;];min;];hour;];days;];weeks;];day;];week;];d;];s;];w;];m;];h;]]==true;{title::x: Greška!}{description:**Netacno napisano vreme.** \n\`$getServerVar[prefix]mute <@user> <vreme> <razlog>\`}{footer:$serverName[]}{color:ff0000}]
 $textSplit[$noMentionMessage[];:]
-$onlyIf[$rolePosition[$highestRole[$client[id]]]<$rolePosition[$roleID[Muted]];{title::x: Greška!}{description:**Moj role nije iznad rola <@&$roleID[Muted]>!**}{footer:Apolo Community™}{color:ff0000}]
-$onlyIf[$rolePosition[$highestRole[$client[id]]]<$rolePosition[$highestRole[$findUser[$message[1]]]];{title::x: Greška!}{description:**Moj role nije iznad rola od <@$findUser[$message[1]]>!**}{footer:Apolo Community™}{color:ff0000}]
-$onlyIf[$roleExists[Muted]==true;{title::x: Greška!}{description:**Muted role ne postoji!**}{footer:Apolo Community™}{color:ff0000}]
-$onlyIf[$userExists[$findUser[$message[1]]]==true;{title::x: Greška!}{description:**Ta osoba ne postoji ili nije na serveru.** \n\`$getServerVar[prefix]mute <@user> <vreme> <razlog>\`}{footer:Apolo Community™}{color:ff0000}]
+$onlyIf[$rolePosition[$highestRole[$client[id]]]<$rolePosition[$roleID[Muted]];{title::x: Greška!}{description:**Moj role nije iznad rola <@&$roleID[Muted]>!**}{footer:$serverName[]}{color:ff0000}]
+$onlyIf[$rolePosition[$highestRole[$client[id]]]<$rolePosition[$highestRole[$findUser[$message[1]]]];{title::x: Greška!}{description:**Moj role nije iznad rola od <@$findUser[$message[1]]>!**}{footer:$serverName[]}{color:ff0000}]
+$onlyIf[$roleExists[Muted]==true;{title::x: Greška!}{description:**Muted role ne postoji!**}{footer:$serverName[]}{color:ff0000}]
+$onlyIf[$userExists[$findUser[$message[1]]]==true;{title::x: Greška!}{description:**Nisi napisao/la koga hoces da mutiras.** \n\`$getServerVar[prefix]mute <@user> <vreme> <razlog>\`}{footer:$serverName[]}{color:ff0000}]
 $onlyIf[$getServerVar[regmodlogs]!={title:Greška}{description:Nije setovan kanal za logovanje. Setuj komandom: **$getServerVar[prefix]setlogs <#taguj kanal>**.}{footer:$serverName[]}{color:ff0000}]
-$onlyBotPerms[manageroles;{title::x: Greška!}{description:**Tebam imati __Manage Roles__ permisiju da bih mutovao!**}{footer:Apolo Community™}{color:ff0000}]
-$onlyForRoles[809542479384281119;809542278774915185;805395974759383042;805249119786369024;805225433191940116;805220500728578088;{title::x: Greška!}{description:**Nemaš dozvolu da koristiš ovu komandu!!!**}{footer:Apolo Community™}{color:ff0000}]
+$onlyBotPerms[manageroles;{title::x: Greška!}{description:**Tebam imati __Manage Roles__ permisiju da bih mutovao!**}{footer:$serverName[]}{color:ff0000}]
+$onlyForRoles[809542479384281119;809542278774915185;805395974759383042;805249119786369024;805225433191940116;805220500728578088;{title::x: Greška!}{description:**Nemaš dozvolu da koristiš ovu komandu!!!**}{footer:$serverName[]}{color:ff0000}]
 
 $suppressErrors[{title:Greška?}{description:Nešto nije u redu. Ako se ovo nastavi, kontaktiraj bot developera.}{color:RED}]
 `
@@ -50,6 +50,6 @@ $suppressErrors[{title:Greška?}{description:Nešto nije u redu. Ako se ovo nast
 //$giveRole[$mentioned[1];$roleID[Muted]]///
 //$takeRole[$mentioned[1];$roleID[Muted]]///
 //$onlyIf[$isNumber[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$message[2];sec;];min;];hour;];days;];wweeks;];day;];week;];d;];s;];w;];m;];h;]]==true;{title::x: Greška!}{description:**Pogrešno napisana komanda. Probaj ovako: \n\`$getServerVar[prefix]mute <@user> *<vreme>* <razlog>\`**}{footer:Apolo Community™}{color:ff0000}]//
-
+//Apolo///
 //$onlyIf[$isNumber[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$message[2];h;];m;];s;];d;];w;];min;];hour;];day;];sec;];week;]]==true;{title::x: Greška!}{description:**Pogrešno napisana komanda. Probaj ovako: \n\`$getServerVar[prefix]mute <@user> *<vreme>* <razlog>\`**}{footer:Apolo Community™}{color:ff0000}]//
 //$onlyIf[$isNumber[$replaceText[$replaceText[$replaceText[$replaceText[$message[2];h;];m;];s;];d;]]==true;Pogresno napisana komanda. Probaj: **!!mute (@username) (razlog)**. () ne trebas da koristis]//
