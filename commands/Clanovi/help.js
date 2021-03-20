586d926ff167ee8915f0c598d8a56b16095b8b8e
Numerto <40825586+RealNumerto@users.noreply.github.com>
2021-03-20 15:07:37 +0100

    Update help.js

--- commands/Clanovi/help.js
+++ commands/Clanovi/help.js
@@ -2,11 +2,11 @@ module.exports = ({
 name: "help",
 code: `
 $title[Pomoc]
-$footer[Apolo Community™;https://media.discordapp.net/attachments/794585038812479539/796688558869446666/gif_apolo.gif]
+$footer[$serverName[];https://media.discordapp.net/attachments/794585038812479539/796688558869446666/gif_apolo.gif]
 $color[$random[0;999999]]
 $description[
-$addField[Komande za membere;<a:point:781135170052292609> **!!help clanovi** (Ovde su sve komande koje clanovi Apolo Community-a mogu da koriste)]
-$addField[Komande za staff;<a:point:781135170052292609> **!!help staff** (Ako si deo staffa moci ces da koristis ovu komandu)]]
+$addField[Komande za membere;<a:rainbow_right:803779739352825867> **!!help clanovi** (Ovde su sve komande koje clanovi Apolo Kingdom-a mogu da koriste)]
+$addField[Komande za staff;<a:rainbow_right:803779739352825867> **!!help staff** (Ako si deo staffa moci ces da koristis ovu komandu)]]
 $onlyIf[$message[1]!=clanovi;{execute:clanovi}]
 $onlyIf[$message[1]!=staff;{execute:staff}]
 $onlyIf[$message[1]!=nema;{execute:nema}]
